68bf72c2b822f9e2afd5e64a357d857a82d230dc
Hugo Roque <hugolnx@gmail.com>
2013-09-09 12:58:39 -0300

    funcaosinha basicona de assert

--- bigint2013-2/main.c
+++ bigint2013-2/main.c
@@ -2,15 +2,38 @@
 #include<stdio.h>
 #include "bigint.h"
 
+int testesPassaram = 0;
+int testesFalharam = 0;
+
+void assert(unsigned int esperado, unsigned int obtido)
+{
+	if (esperado == obtido)
+	{
+		testesPassaram++;
+	}
+	else
+	{
+		testesFalharam++;
+		printf("esperado %d, mas foi obtido %d\n", esperado, obtido);
+	}
+}
+
+void reportarTestes()
+{
+	printf("Passaram: %d    Falharam: %d\n", testesPassaram, testesFalharam);
+}
+
+
 int main() {
 	BigInt big;
 	unsigned int x;
 
+	// big_uval e cast_uint
 	big_uval(big, 10000);
-	
 	x = big_cast_uint(big);
-
-	printf("%d\n" , x);
+	assert(10000, x);
 	
+	reportarTestes();
+
 	return 0;
 }
