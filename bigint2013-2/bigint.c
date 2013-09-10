741c0f93a58a43a86c61852eb0fec0eeba266772
Hugo Roque <hugolnx@gmail.com>
2013-09-10 14:41:37 -0300

    valores ocupando os bits menos significativos primeiro

--- bigint2013-2/bigint.c
+++ bigint2013-2/bigint.c
@@ -3,22 +3,22 @@ typedef unsigned char BigInt[NUM_BYTES];
 
 void big_val (BigInt res, int val)
 {
-	int * n = (int *) res;
+	int * n = (int *) (res + 16 - 4);
 	*n = val;
 }
 
 void big_uval (BigInt res, unsigned int uval)
 {
-	unsigned int * n = (unsigned int *) res; 
+	unsigned int * n = (unsigned int *) (res + 16 - 4); 
 	*n = uval;
 }
 
 unsigned int big_cast_uint(BigInt res)
 {
-	return *((unsigned int *) res);
+	return *((unsigned int *) (res + 16 - 4));
 }
 
 int big_cast_int(BigInt res)
 {
-	return *((int *) res);
+	return *((int *) (res + 16 - 4));
 }
