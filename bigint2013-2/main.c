7d2f55839e62c6637cb734832d9e99b05a3f9d94
Hugo Roque <hugolnx@gmail.com>
2013-09-10 14:56:52 -0300

    melhorando api de testes

--- bigint2013-2/main.c
+++ bigint2013-2/main.c
@@ -2,44 +2,50 @@
 #include<stdio.h>
 #include "bigint.h"
 
-int testesPassaram = 0;
-int testesFalharam = 0;
+int assertsPassaram = 0;
+int assertsFalharam = 0;
+
+char * testName;
 
 void assert(unsigned int esperado, unsigned int obtido)
 {
 	if (esperado == obtido)
 	{
-		testesPassaram++;
+		assertsPassaram++;
 	}
 	else
 	{
-		testesFalharam++;
-		printf("esperado %d, mas foi obtido %d\n", esperado, obtido);
+		assertsFalharam++;
+		printf("[%s]: esperado %d, mas foi obtido %d\n", testName, esperado, obtido);
 	}
+}
+
+void test(char * name)
+{
+	testName = name;
 }
 
 void reportarTestes()
 {
-	printf("Passaram: %d    Falharam: %d\n", testesPassaram, testesFalharam);
+	printf("Passaram: %d    Falharam: %d\n", assertsPassaram, assertsFalharam);
 }
-
 
 int main() {
 	BigInt big;
 	unsigned int uint;
 	int vint;
 
-	// big_uval e cast_uint
+	test("cast_uint após big_uval tem que permanecer o mesmo valor");
 	big_uval(big, 33);
 	uint = big_cast_uint(big);
 	assert(33, uint);
 
-	// big_val e cast_int
+	test("cast_int após big_uval tem que permanecer o mesmo valor");
 	big_val(big, 33);
 	vint = big_cast_int(big);
 	assert(33, vint);
 
-	// big_val e cast_int com negativo
+	test("cast_int após big_val com negativo tem que permanecer o mesmo valor");
 	big_val(big, -33);
 	vint = big_cast_int(big);
 	assert(-33, vint);
