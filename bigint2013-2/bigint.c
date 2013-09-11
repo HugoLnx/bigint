63e70ef50c882c2bfd8a014eaa6dffea871f4a1b
Hugo Roque <hugolnx@gmail.com>
2013-09-11 16:56:23 -0300

    corrigindo big_uval e big_val para botar como os primeiros bits

--- bigint2013-2/bigint.c
+++ bigint2013-2/bigint.c
@@ -1,24 +1,41 @@
+#include<stdio.h>
+
 #define NUM_BYTES 16
 typedef unsigned char BigInt[NUM_BYTES];
 
+void zerarBytes(BigInt res);
+
 void big_val (BigInt res, int val)
 {
-	int * n = (int *) (res + 16 - 4);
+	int * n = (int *) res;
+	zerarBytes(res);
 	*n = val;
 }
 
 void big_uval (BigInt res, unsigned int uval)
 {
-	unsigned int * n = (unsigned int *) (res + 16 - 4); 
+	unsigned int * n = (unsigned int *) res; 
+	zerarBytes(res);
 	*n = uval;
 }
 
 unsigned int big_cast_uint(BigInt res)
 {
-	return *((unsigned int *) (res + 16 - 4));
+	return *((unsigned int *) res);
 }
 
 int big_cast_int(BigInt res)
 {
-	return *((int *) (res + 16 - 4));
+	return *((int *) res);
+}
+
+void zerarBytes(BigInt res)
+{
+	int i;
+	char * bytes = (char *) res;
+
+	for ( i = 0 ; i < NUM_BYTES ; i++ )
+	{
+		*bytes++ = 0;
+	}
 }
--- bigint2013-2/main.c
+++ bigint2013-2/main.c
@@ -16,7 +16,34 @@ void assert(unsigned int esperado, unsigned int obtido)
 	else
 	{
 		assertsFalharam++;
-		printf("[%s]: esperado %d, mas foi obtido %d\n", testName, esperado, obtido);
+		printf("[%s]: esperado %02x, mas foi obtido %02x\n", testName, esperado, obtido);
+	}
+}
+
+void assertBytes(char * esperado, void * obtido)
+{
+	int i;
+	int erro = 0;
+	char * bytesObt = (char *) obtido;
+	char * bytesEsp = esperado;
+
+	for ( i = 0 ; i < 16 ; i++ )
+	{
+		erro = (*bytesEsp++ != *bytesObt++);
+		if(erro) break;
+	}
+
+	if(erro)
+	{
+		bytesObt = (char *) obtido;
+		bytesEsp = esperado;
+
+		
+		printf("[%s]: erro bytes errados:\nesperado > obtido\n", testName);
+		for ( i = 0 ; i < 16 ; i++ )
+		{
+			printf("%02x > %02x\n", *bytesEsp++, *bytesObt++);
+		}
 	}
 }
 
@@ -30,8 +57,24 @@ void reportarTestes()
 	printf("Passaram: %d    Falharam: %d\n", assertsPassaram, assertsFalharam);
 }
 
+char * bytesFrom(char b1, char b2, char b3, char b4,
+	char b5, char b6, char b7, char b8,
+	char b9, char b10, char b11, char b12,
+	char b13, char b14, char b15, char b16)
+{
+	char * bytes = (char *) malloc(sizeof(char)*16);
+	char * bytesBegin = bytes;
+
+	*(bytes++) = b1; *(bytes++) = b2; *(bytes++) = b3; *(bytes++) = b4;
+	*(bytes++) = b5; *(bytes++) = b6; *(bytes++) = b7; *(bytes++) = b8;
+	*(bytes++) = b9; *(bytes++) = b10; *(bytes++) = b11; *(bytes++) = b12;
+	*(bytes++) = b13; *(bytes++) = b14; *(bytes++) = b15; *(bytes++) = b16;
+
+	return bytesBegin;
+}
+
 int main() {
-	BigInt big;
+	BigInt big, a, b;
 	unsigned int uint;
 	int vint;
 
@@ -49,6 +92,24 @@ int main() {
 	big_val(big, -33);
 	vint = big_cast_int(big);
 	assert(-33, vint);
+
+	test("big_uval => 1 como: 0x00(15x) 0x01");
+	big_uval(big, 1);
+	assertBytes(bytesFrom(
+		0x01, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00
+	), big);
+	
+	test("big_val => 1 como: 0x00(15x) 0x01");
+	big_val(big, 1);
+	assertBytes(bytesFrom(
+		0x01, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00
+	), big);
 
 	reportarTestes();
 
